b26ed2176c2048a03eb3a2619e2e80101f343a29
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2021-04-20 02:46:30 -0400

    Fix dasboard

--- app/api/routes/compra/route.js
+++ app/api/routes/compra/route.js
@@ -6,7 +6,5 @@ const router = Router();
 
 router.get('/', handler.getCompra);
 router.post('/add', handler.addCompra);
-// router.put('/', handler.updateUser);
-// router.delete('/', handler.deleteUser);
 
 module.exports = router;
